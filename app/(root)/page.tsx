a594352bd3f8eae5f6b0ac75195c3a1984c13282
Teju Thomas <thomasteju7@gmail.com>
2024-06-30 18:34:17 +0530

    implement DoughnutChart and Welcome message

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,8 +1,32 @@
-import React from 'react'
+"use client"
+
+import HeaderBox from '@/components/HeaderBox'
+import TotalBalanceBox from '@/components/TotalBalanceBox';
 
 const Home = () => {
+  const loggedIn = { firstName: 'Teju'};
   return (
-    <div>Home</div>
+    <section className='home'>
+
+    <div className='home-content'>
+      <header className='home-header'>
+        <HeaderBox 
+        type="greeting"
+        title="Welcome"
+        user={loggedIn?.firstName || 'Guest'}
+        subtext="Access and manage your account and transactions efficiently."
+        />
+
+        <TotalBalanceBox
+          accounts={[]}
+          totalBanks={1}
+          totalCurrentBalance={1250.35} 
+        />
+
+
+      </header>
+    </div>
+    </section>
   )
 }
 
